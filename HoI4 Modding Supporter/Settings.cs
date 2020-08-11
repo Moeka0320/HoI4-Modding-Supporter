9715d3f6d84156a01dec27bd433342752e9ed898
Zakki <zakki0925224@gmail.com>
2020-08-11 19:05:05 +0900

    HoI4本体ディレクトリ指定時の検査を実装

--- HoI4 Modding Supporter/Settings.cs
+++ HoI4 Modding Supporter/Settings.cs
@@ -49,14 +49,22 @@ namespace HoI4_Modding_Supporter
             }
             else
             {
-                // ここで設定を保存
-                Properties.Settings.Default.hoi4dir = textBox1.Text;
-                Properties.Settings.Default.moddir = textBox2.Text;
-                Properties.Settings.Default.afterOpenFolder = checkBox1.Checked;
+                // HoI4本体ディレクトリが本当に合っているか（commonディレクトリとgfxディレクトリとhoi4.exeの存在確認）
+                if (Directory.Exists(textBox1.Text + @"\common") == true && Directory.Exists(textBox1.Text + @"\gfx") == true && File.Exists(textBox1.Text + @"\hoi4.exe") == true) 
+                {
+                    // ここで設定を保存
+                    Properties.Settings.Default.hoi4dir = textBox1.Text;
+                    Properties.Settings.Default.moddir = textBox2.Text;
+                    Properties.Settings.Default.afterOpenFolder = checkBox1.Checked;
 
-                Properties.Settings.Default.Save();
+                    Properties.Settings.Default.Save();
 
-                this.Close();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("HoI4本体のディレクトリが間違っています。", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
             }
         }
     }
