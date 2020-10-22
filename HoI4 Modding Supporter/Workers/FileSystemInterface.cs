8b23fd7583ee2fed397b1d2e059f8c8bd85c07fd
Zakki <zakki0925224@gmail.com>
2020-10-22 20:38:54 +0900

    [Add]: Adding methods related to file operations

--- HoI4 Modding Supporter/Workers/FileSystemInterface.cs
+++ HoI4 Modding Supporter/Workers/FileSystemInterface.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,7 +18,8 @@ namespace HoI4_Modding_Supporter.Workers
         /// <summary>
         /// ファイルをコピー
         /// </summary>
-        /// <returns></returns>
+        /// <param name="source"></param>
+        /// <param name="dest"></param>
         public int FileCopy(string source, string dest)
         {
             try
@@ -85,6 +87,84 @@ namespace HoI4_Modding_Supporter.Workers
                                       e is DirectoryNotFoundException ||
                                       e is IOException ||
                                       e is NotSupportedException)
+            {
+                mbs.ErrorMessage(e.Message);
+                return 1;
+            }
+
+            return 0;
+        }
+
+        /// <summary>
+        /// 末尾にテキストを一行書き込む
+        /// </summary>
+        /// <param name="path">ファイルパス</param>
+        /// <param name="text">書き込みしたいテキスト</param>
+        /// <param name="parm">true -> 既存テキストを残したまま末尾に追加<br/>false -> 既存テキストを抹消した後に追加</param>
+        /// <param name="enc">エンコード指定（未指定でも可）<br/>UTF-8が指定された場合はBOM付きになる</param>
+        /// <returns></returns>
+        public int FileWriteLine(string path, string text, bool parm, Encoding enc = null)
+        {
+            
+            try
+            {
+                StreamWriter sw;
+
+                if (enc == null)
+                    sw = new StreamWriter(path, parm);
+                else
+                    sw = new StreamWriter(path, parm, enc);
+
+                sw.WriteLine(text);
+                sw.Close();
+            }
+            catch (Exception e) when (e is UnauthorizedAccessException ||
+                                      e is ArgumentException ||
+                                      e is ArgumentNullException ||
+                                      e is DirectoryNotFoundException ||
+                                      e is IOException ||
+                                      e is PathTooLongException ||
+                                      e is SecurityException ||
+                                      e is ObjectDisposedException)
+            {
+                mbs.ErrorMessage(e.Message);
+                return 1;
+            }
+
+            return 0;
+        }
+
+        /// <summary>
+        /// 末尾にテキストを書き込む
+        /// </summary>
+        /// <param name="path">ファイルパス</param>
+        /// <param name="text">書き込みしたいテキスト</param>
+        /// <param name="parm">true -> 既存テキストを残したまま末尾に追加<br/>false -> 既存テキストを抹消した後に追加</param>
+        /// <param name="enc">エンコード指定（未指定でも可）<br/>UTF-8が指定された場合はBOM付きになる</param>
+        /// <returns></returns>
+        public int FileWrite(string path, string text, bool parm, Encoding enc = null)
+        {
+
+            try
+            {
+                StreamWriter sw;
+
+                if (enc == null)
+                    sw = new StreamWriter(path, parm);
+                else
+                    sw = new StreamWriter(path, parm, enc);
+
+                sw.Write(text);
+                sw.Close();
+            }
+            catch (Exception e) when (e is UnauthorizedAccessException ||
+                                      e is ArgumentException ||
+                                      e is ArgumentNullException ||
+                                      e is DirectoryNotFoundException ||
+                                      e is IOException ||
+                                      e is PathTooLongException ||
+                                      e is SecurityException ||
+                                      e is ObjectDisposedException)
             {
                 mbs.ErrorMessage(e.Message);
                 return 1;
