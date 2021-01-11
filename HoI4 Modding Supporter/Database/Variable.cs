42bf32026c7aa4d60e6f7455b69789a05daa7e3e
Zakki0925224 <zakki0925224@gmail.com>
2021-01-11 11:23:09 +0900

    [Improve]: Corrected property description

--- HoI4 Modding Supporter/Database/Variable.cs
+++ HoI4 Modding Supporter/Database/Variable.cs
@@ -4,513 +4,152 @@
     class Variable
     {
         // Application Settings
-        private static string hoi4dir;
-        public string Hoi4dir
-        {
-            set { hoi4dir = value; }
-            get { return hoi4dir; }
-        }
+        public string Hoi4dir { get; set; }
 
-        private static string moddir;
-        public string Moddir
-        {
-            set { moddir = value; }
-            get { return moddir; }
-        }
+        public string Moddir { get; set; }
 
         // Main
-        private static string countryTag;
-        public string CountryTag
-        {
-            set { countryTag = value; }
-            get { return countryTag; }
-        }
+        public string CountryTag { get; set; }
 
-        private static string countryName;
-        public string CountryName
-        {
-            set { countryName = value; }
-            get { return countryName; }
-        } 
+        public string CountryName { get; set; }
 
-        private static string n_ViewName;
-        public string N_ViewName
-        {
-            set { n_ViewName = value; }
-            get { return n_ViewName; }
-        }
+        public string N_ViewName { get; set; }
 
-        private static string n_EventViewName;
-        public string N_EventViewName
-        {
-            set { n_EventViewName = value; }
-            get { return n_EventViewName; }
-        }
+        public string N_EventViewName { get; set; }
 
-        private static string n_AliasName;
-        public string N_AliasName
-        {
-            set { n_AliasName = value; }
-            get { return n_AliasName; }
-        }
+        public string N_AliasName { get; set; }
 
-        private static string d_ViewName;
-        public string D_ViewName
-        {
-            set { d_ViewName = value; }
-            get { return d_ViewName; }
-        }
+        public string D_ViewName { get; set; }
 
-        private static string d_EventViewName;
-        public string D_EventViewName
-        {
-            set { d_EventViewName = value; }
-            get { return d_EventViewName; }
-        }
+        public string D_EventViewName { get; set; }
 
-        private static string d_AliasName;
-        public string D_AliasName
-        {
-            set { d_AliasName = value; }
-            get { return d_AliasName; }
-        }
+        public string D_AliasName { get; set; }
 
-        private static string f_ViewName;
-        public string F_ViewName
-        {
-            set { f_ViewName = value; }
-            get { return f_ViewName; }
-        }
+        public string F_ViewName { get; set; }
 
-        private static string f_EventViewName;
-        public string F_EventViewName
-        {
-            set { f_EventViewName = value; }
-            get { return f_EventViewName; }
-        }
+        public string F_EventViewName { get; set; }
 
-        private static string f_AliasName;
-        public string F_AliasName
-        {
-            set { f_AliasName = value; }
-            get { return f_AliasName; }
-        }
+        public string F_AliasName { get; set; }
 
-        private static string c_ViewName;
-        public string C_ViewName
-        {
-            set { c_ViewName = value; }
-            get { return c_ViewName; }
-        }
+        public string C_ViewName { get; set; }
 
-        private static string c_EventViewName;
-        public string C_EventViewName
-        {
-            set { c_EventViewName = value; }
-            get { return c_EventViewName; }
-        }
+        public string C_EventViewName { get; set; }
 
-        private static string c_AliasName;
-        public string C_AliasName
-        {
-            set { c_AliasName = value; }
-            get { return c_AliasName; }
-        }
+        public string C_AliasName { get; set; }
 
-        private static string n_FlagBig;
-        public string N_FlagBig
-        {
-            set { n_FlagBig = value; }
-            get { return n_FlagBig; }
-        }
+        public string N_FlagBig { get; set; }
 
-        private static string n_FlagMed;
-        public string N_FlagMed
-        {
-            set { n_FlagMed = value; }
-            get { return n_FlagMed; }
-        }
+        public string N_FlagMed { get; set; }
 
-        private static string n_FlagSma;
-        public string N_FlagSma
-        {
-            set { n_FlagSma = value; }
-            get { return n_FlagSma; }
-        }
+        public string N_FlagSma { get; set; }
 
-        private static string d_FlagBig;
-        public string D_FlagBig
-        {
-            set { d_FlagBig = value; }
-            get { return d_FlagBig; }
-        }
+        public string D_FlagBig { get; set; }
 
-        private static string d_FlagMed;
-        public string D_FlagMed
-        {
-            set { d_FlagMed = value; }
-            get { return d_FlagMed; }
-        }
+        public string D_FlagMed { get; set; }
 
-        private static string d_FlagSma;
-        public string D_FlagSma
-        {
-            set { d_FlagSma = value; }
-            get { return d_FlagSma; }
-        }
+        public string D_FlagSma { get; set; }
 
-        private static string f_FlagBig;
-        public string F_FlagBig
-        {
-            set { f_FlagBig = value; }
-            get { return f_FlagBig; }
-        }
+        public string F_FlagBig { get; set; }
 
-        private static string f_FlagMed;
-        public string F_FlagMed
-        {
-            set { f_FlagMed = value; }
-            get { return f_FlagMed; }
-        }
+        public string F_FlagMed { get; set; }
 
-        private static string f_FlagSma;
-        public string F_FlagSma
-        {
-            set { f_FlagSma = value; }
-            get { return f_FlagSma; }
-        }
+        public string F_FlagSma { get; set; }
 
-        private static string c_FlagBig;
-        public string C_FlagBig
-        {
-            set { c_FlagBig = value; }
-            get { return c_FlagBig; }
-        }
+        public string C_FlagBig { get; set; }
 
-        private static string c_FlagMed;
-        public string C_FlagMed
-        {
-            set { c_FlagMed = value; }
-            get { return c_FlagMed; }
-        }
+        public string C_FlagMed { get; set; }
 
-        private static string c_FlagSma;
-        public string C_FlagSma
-        {
-            set { c_FlagSma = value; }
-            get { return c_FlagSma; }
-        }
+        public string C_FlagSma { get; set; }
 
-        private static string n_PartyAliasName;
-        public string N_PartyAliasName
-        {
-            set { n_PartyAliasName = value; }
-            get { return n_PartyAliasName; }
-        }
+        public string N_PartyAliasName { get; set; }
 
-        private static string n_PartyFullName;
-        public string N_PartyFullName
-        {
-            set { n_PartyFullName = value; }
-            get { return n_PartyFullName; }
-        }
+        public string N_PartyFullName { get; set; }
 
-        private static string d_PartyAliasName;
-        public string D_PartyAliasName
-        {
-            set { d_PartyAliasName = value; }
-            get { return d_PartyAliasName; }
-        }
+        public string D_PartyAliasName { get; set; }
 
-        private static string d_PartyFullName;
-        public string D_PartyFullName
-        {
-            set { d_PartyFullName = value; }
-            get { return d_PartyFullName; }
-        }
+        public string D_PartyFullName { get; set; }
 
-        private static string f_PartyAliasName;
-        public string F_PartyAliasName
-        {
-            set { f_PartyAliasName = value; }
-            get { return f_PartyAliasName; }
-        }
+        public string F_PartyAliasName { get; set; }
 
-        private static string f_PartyFullName;
-        public string F_PartyFullName
-        {
-            set { f_PartyFullName = value; }
-            get { return f_PartyFullName; }
-        }
+        public string F_PartyFullName { get; set; }
 
-        private static string c_PartyAliasName;
-        public string C_PartyAliasName
-        {
-            set { c_PartyAliasName = value; }
-            get { return c_PartyAliasName; }
-        }
+        public string C_PartyAliasName { get; set; }
 
-        private static string c_PartyFullName;
-        public string C_PartyFullName
-        {
-            set { c_PartyFullName = value; }
-            get { return c_PartyFullName; }
-        }
+        public string C_PartyFullName { get; set; }
 
-        private static int colorR;
-        public int ColorR
-        {
-            set { colorR = value; }
-            get { return colorR; }
-        }
+        public int ColorR { get; set; }
 
-        private static int colorG;
-        public int ColorG
-        {
-            set { colorG = value; }
-            get { return colorG; }
-        }
+        public int ColorG { get; set; }
 
-        private static int colorB;
-        public int ColorB
-        {
-            set { colorB = value; }
-            get { return colorB; }
-        }
+        public int ColorB { get; set; }
 
-        private static string graphicalCulture;
-        public string GraphicalCulture
-        {
-            set { graphicalCulture = value; }
-            get { return graphicalCulture; }
-        }
+        public string GraphicalCulture { get; set; }
 
-        private static string graphicalCulture2d;
-        public string GraphicalCulture2d
-        {
-            set { graphicalCulture2d = value; }
-            get { return graphicalCulture2d; }
-        }
+        public string GraphicalCulture2d { get; set; }
 
-        private static int stateIDWithCapital;
-        public int StateIDWithCapital
-        {
-            set { stateIDWithCapital = value; }
-            get { return stateIDWithCapital; }
-        }
+        public int StateIDWithCapital { get; set; }
 
-        private static int studySlot;
-        public int StudySlot
-        {
-            set { studySlot = value; }
-            get { return studySlot; }
-        }
+        public int StudySlot { get; set; }
 
-        private static double stability;
-        public double Stability
-        {
-            set { stability = value; }
-            get { return stability; }
-        }
+        public double Stability { get; set; }
 
-        private static double warCoop;
-        public double WarCoop
-        {
-            set { warCoop = value; }
-            get { return warCoop; }
-        }
+        public double WarCoop { get; set; }
 
-        private static int politicalPower;
-        public int PoliticalPower
-        {
-            set { politicalPower = value; }
-            get { return politicalPower; }
-        }
+        public int PoliticalPower { get; set; }
 
-        private static int transportShip;
-        public int TransportShip
-        {
-            set { transportShip = value; }
-            get { return transportShip; }
-        }
+        public int TransportShip { get; set; }
 
-        private static bool dependentCountry;
-        public bool DependentCountry
-        {
-            set { dependentCountry = value; }
-            get { return dependentCountry; }
-        }
+        public bool DependentCountry { get; set; }
 
-        private static string sovereignCountryTag;
-        public string SovereignCountryTag
-        {
-            set { sovereignCountryTag = value; }
-            get { return sovereignCountryTag; }
-        }
+        public string SovereignCountryTag { get; set; }
 
-        private static int n_Popularity;
-        public int N_Popularity
-        {
-            set { n_Popularity = value; }
-            get { return n_Popularity; }
-        }
+        public int N_Popularity { get; set; }
 
-        private static int d_Popularity;
-        public int D_Popularity
-        {
-            set { d_Popularity = value; }
-            get { return d_Popularity; }
-        }
+        public int D_Popularity { get; set; }
 
-        private static int f_Popularity;
-        public int F_Popularity
-        {
-            set { f_Popularity = value; }
-            get { return f_Popularity; }
-        }
+        public int F_Popularity { get; set; }
 
-        private static int c_Popularity;
-        public int C_Popularity
-        {
-            set { c_Popularity = value; }
-            get { return c_Popularity; }
-        }
+        public int C_Popularity { get; set; }
 
-        private static string startIdeology;
-        public string StartIdeology
-        {
-            set { startIdeology = value; }
-            get { return startIdeology; }
-        }
+        public string StartIdeology { get; set; }
 
-        private static int lastElectionYYYY;
-        public int LastElectionYYYY
-        {
-            set { lastElectionYYYY = value; }
-            get { return lastElectionYYYY; }
-        }
+        public int LastElectionYYYY { get; set; }
 
-        private static int lastElectionM;
-        public int LastElectionM
-        {
-            set { lastElectionM = value; }
-            get { return lastElectionM; }
-        }
+        public int LastElectionM { get; set; }
 
-        private static int lastElectionD;
-        public int LastElectionD
-        {
-            set { lastElectionD = value; }
-            get { return lastElectionD; }
-        }
+        public int LastElectionD { get; set; }
 
-        private static string lastElection;
-        public string LastElection
-        {
-            set { lastElection = value; }
-            get { return lastElection; }
-        }
+        public string LastElection { get; set; }
 
-        private static int electionFrequency;
-        public int ElectionFrequency
-        {
-            set { electionFrequency = value; }
-            get { return electionFrequency; }
-        }
+        public int ElectionFrequency { get; set; }
 
-        private static bool noElection;
-        public bool NoElection
-        {
-            set { noElection = value; }
-            get { return noElection; }
-        }
+        public bool NoElection { get; set; }
 
-        private static string modName;
-        public string ModName
-        {
-            set { modName = value; }
-            get { return modName; }
-        }
+        public string ModName { get; set; }
 
         // NationalLeaderSettings
-        private static bool customLeaderEnabled;
-        public bool CustomLeaderEnabled
-        {
-            set { customLeaderEnabled = value; }
-            get { return customLeaderEnabled; }
-        }
+        public bool CustomLeaderEnabled { get; set; }
 
-        private static string leaderName;
-        public string LeaderName
-        {
-            set { leaderName = value; }
-            get { return leaderName; }
-        }
+        public string LeaderName { get; set; }
 
-        private static string leaderDesc;
-        public string LeaderDesc
-        {
-            set { leaderDesc = value; }
-            get { return leaderDesc; }
-        }
+        public string LeaderDesc { get; set; }
 
-        private static string leaderPicturePath;
-        public string LeaderPicturePath
-        {
-            set { leaderPicturePath = value; }
-            get { return leaderPicturePath; }
-        }
+        public string LeaderPicturePath { get; set; }
 
-        private static string leaderPictureName;
-        public string LeaderPictureName
-        {
-            set { leaderPictureName = value; }
-            get { return leaderPictureName; }
-        }
+        public string LeaderPictureName { get; set; }
 
-        private static string leaderIdeology;
-        public string LeaderIdeology
-        {
-            set { leaderIdeology = value; }
-            get { return leaderIdeology; }
-        }
+        public string LeaderIdeology { get; set; }
 
-        private static string willNotAppear;
-        public string WillNotAppear
-        {
-            set { willNotAppear = value; }
-            get { return willNotAppear; }
-        }
+        public string WillNotAppear { get; set; }
 
         // FactionSettings
+        public bool FactionCreateEnabled { get; set; }
 
-        private static bool factionCreateEnabled;
-        public bool FactionCreateEnabled
-        { 
-            set { factionCreateEnabled = value; }
-            get { return factionCreateEnabled; }
-        }
+        public string FactionInternalName { get; set; }
 
-        private static string factionInternalName;
-        public string FactionInternalName
-        {
-            set { factionInternalName = value; }
-            get { return factionInternalName; }
-        }
+        public string FactionName { get; set; }
 
-        private static string factionName;
-        public string FactionName
-        {
-            set { factionName = value; }
-            get { return factionName; }
-        }
-
-        private static string[] factionParticipatingCountries;
-        public string[] FactionParticipatingCountries
-        {
-            set { factionParticipatingCountries = value; }
-            get { return factionParticipatingCountries; }
-        }
+        public string[] FactionParticipatingCountries { get; set; }
 
         // CustomIdeology (配列の順序は customIdeologiesInternalName / customIdeologiesName に準拠)
         /*
@@ -524,20 +163,8 @@
             [TABCOUNT - 1, 7] = partyFullNameTextBox.Text 
         */
 
-        private static string[,] customIdeologiesSettings;
+        public string[,] CustomIdeologiesSettings { get; set; }
 
-        public string[,] CustomIdeologiesSettings
-        {
-            set { customIdeologiesSettings = value; }
-            get { return customIdeologiesSettings; }
-        }
-
-        private static int[] customIdeologiesPopularity;
-
-        public int[] CustomIdeologiesPopularity
-        {
-            set { customIdeologiesPopularity = value; }
-            get { return customIdeologiesPopularity; }
-        }
+        public int[] CustomIdeologiesPopularity { get; set; }
     }
 }
